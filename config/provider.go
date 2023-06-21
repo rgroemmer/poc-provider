956ad533015c33cf60ea7e710b1703eade3690bb
Raphael Groemmer <raphael.groemmer@stackit.de>
2023-06-21 13:52:18 +0200

    run hack/prepare.sh

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/rgroemmer/provider-poc/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "poc"
+	modulePath     = "github.com/rgroemmer/provider-poc"
 )
 
 //go:embed schema.json
